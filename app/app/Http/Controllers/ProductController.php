20498eadaf500af2e7d3a86a8efed8000276b2f5
Magomed Aminov <ledmaggazin@gmail.com>
2022-10-30 00:40:12 +0200

    feat: add api products

--- app/app/Http/Controllers/ProductController.php
+++ app/app/Http/Controllers/ProductController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -14,8 +17,22 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = DB::table('products')
+            ->select(['products.id',
+                    'products.name',
+                    'products.url',
+                    'categories.name AS category',
+                    'collections.name AS collection',
+                    'products.description_en',
+                    'products.description_es',
+                    'products.description_ru',
+                    'products.price',])
+            ->leftJoin('categories', 'products.category_id', '=', 'categories.id')
+            ->leftJoin('collections', 'products.collection_id', '=', 'collections.id')
+            ->get(); 
+        return response()->json($products);
     }
+
 
     /**
      * Show the form for creating a new resource.
@@ -35,18 +52,25 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        $product = new Product();
-        
-        $product->name = $request->name;
-        $product->category = $request->category;
-        $product->collection = $request->collection;
-        $product->url = $request->url;
-        $product->description_en = $request->description_en;
-        $product->description_en = $request->description_es;
-        $product->description_en = $request->description_ru;
+        $request->all();
 
+        $dir = 'products/' . $request->name . '/';
+
+        $image = $request->file('image')->store($dir, 'public');
+
+        $product = new Product();
+ 
+        $product->name = $request->name;
+        $product->url = $image;
+        $product->description_en = $request->description_en;
+        $product->description_es = $request->description_es;
+        $product->description_ru = $request->description_ru;
+        $product->price = $request->price;
+        $product->category_id = $request->category;
+        $product->collection_id = $request->collection;
         $product->save();
 
+        return response()->status(200);
     }
 
     /**
@@ -57,7 +81,8 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        //
+        $product = Product::where('id', $id)->first();
+        return response()->json($product);
     }
 
     /**
@@ -80,7 +105,39 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->all();
+
+        $product = Product::find($id);
+
+        if($product->name !== $request->name){
+            $fileName = pathinfo($product->url, PATHINFO_FILENAME);
+            $extension = pathinfo($product->url, PATHINFO_EXTENSION);
+            $fileName = $fileName . '.' . $extension;
+            // Storage::disk('public')->delete('products/' . $request->name . '/' . $fileName);
+            $dir = 'products/' . $request->name . '/' . $fileName;
+            Storage::disk('public')->move('products/' . $product->name, 'products/' . $request->name);
+            $product->name = $request->name;
+            $product->url = $dir;
+        }
+        if($request->file('image')){
+            // $fileName = pathinfo($product->url, PATHINFO_FILENAME);
+            // $extension = pathinfo($product->url, PATHINFO_EXTENSION);
+            // $fileName = $fileName . '.' . $extension;
+            // Storage::disk('public')->delete('products/' . $request->name . '/' . $fileName);
+            $dir = 'products/' . $request->name . '/';
+            $image = $request->file('image')->store($dir, 'public');
+            $product->url = $image;
+        }
+        $product->description_en = $request->description_en;
+        $product->description_es = $request->description_es;
+        $product->description_ru = $request->description_ru;
+        $product->price = $request->price;
+        $product->category_id = $request->category_id;
+        $product->collection_id = $request->collection_id;
+
+        $product->save();
+
+        return response()->status(200);
     }
 
     /**
@@ -91,6 +148,60 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $product = Product::where('id', $id)->first();
+        Storage::disk('public')->deleteDirectory('products/' . $product->name);
+        Product::destroy($id);
+        return response()->status(200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function sort(Request $request)
+    {
+        $bodyContent = $request->getContent();
+
+        $data = json_decode($bodyContent);
+
+        $name = $data->name;
+        $category = $data->category;
+        $collection = $data->collection;
+        $minPrice = $data->minPrice;
+        $maxPrice = $data->maxPrice;
+        $order = $data->order; // desc
+
+        $conditions = [];
+
+        if($name){
+            array_push($conditions, ['products.name', 'LIKE', $name]);
+        };
+        if($category){
+            array_push($conditions, ['products.category_id', '=', $category]);
+        };
+        if($collection){
+            array_push($conditions, ['products.collection_id', '=', $collection]);
+        };
+
+        $products = DB::table('products')
+            ->select(['products.id',
+                    'products.name',
+                    'products.url',
+                    'categories.name AS category',
+                    'collections.name AS collection',
+                    'products.description_en',
+                    'products.description_es',
+                    'products.description_ru',
+                    'products.price',])
+            ->where($conditions)
+            ->whereBetween('products.price', [$minPrice, $maxPrice])
+            ->orderBy('products.name', $order)
+            ->leftJoin('categories', 'products.category_id', '=', 'categories.id')
+            ->leftJoin('collections', 'products.collection_id', '=', 'collections.id')
+            ->get(); 
+
+        return response()->json($products);
     }
 }
--- app/routes/api.php
+++ app/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('products', [ProductController::class, 'index'])->name('products');
+
+Route::post('products/sort', [ProductController::class, 'sort'])->name('products.sort');
+
+Route::get('products/{id}', [ProductController::class, 'show'])->name('products.show');
+
+Route::post('products/create', [ProductController::class, 'store'])->name('products.create');
+
+Route::patch('products/update/{id}', [ProductController::class, 'update'])->name('products.update');
+
+Route::delete('products/delete/{id}', [ProductController::class, 'delete'])->name('products.delete');
